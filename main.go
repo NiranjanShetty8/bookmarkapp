459acdb340cf96c9fd3a64ef7e34153e2d45ebeb
Niranjan <shetty.niranjan4@gmail.com>
2020-03-23 02:44:04 +0530

    removed redundant comments.

--- main.go
+++ main.go
@@ -59,11 +59,11 @@ func main() {
 		fmt.Println("Closing DB")
 		db.Close()
 	}()
-	// db.Close()
 	fmt.Println("Server ShutDown....")
 	os.Exit(0)
 }
 
+// Create instance of services & controllers & register routes
 func initialize(db *gorm.DB, repos *repository.GormRepository, router *mux.Router) {
 	userService := services.NewUserService(db, repos)
 	categoryService := services.NewCategoryService(db, repos)
@@ -77,84 +77,3 @@ func initialize(db *gorm.DB, repos *repository.GormRepository, router *mux.Route
 	categoryController.RegisterRoutes(router)
 	bookmarkController.RegisterRoutes(router)
 }
-
-// repos := repository.NewGormRepository()
-// us := services.NewUserService(db, repos)
-// cs := services.NewCategoryService(db, repos)
-// bms := services.NewBookmarkService(db, repos)
-
-// userGenId := uuid.NewV4()
-// categoryGenId := uuid.NewV4()
-
-// bookmark1 := model.Bookmark{
-// 	Description: "google",
-// 	URL:         "www.google.com",
-// 	CategoryID:  categoryGenId,
-// }
-
-// bookmark2 := model.Bookmark{
-// 	Description: "yahoo",
-// 	URL:         "www.yahoo.com",
-// 	CategoryID:  categoryGenId,
-// }
-// // allBookMarks := []model.Bookmark{bookmark1, bookmark2}
-
-// category1 := model.Category{
-// 	Base: model.Base{
-// 		ID: categoryGenId,
-// 	},
-// 	Name: "Social",
-// 	// Bookmarks: allBookMarks,
-// 	UserID: userGenId,
-// }
-// // allCategories := []model.Category{category1}
-// user := model.User{
-// 	Base: model.Base{
-// 		ID: userGenId,
-// 	},
-// 	Username: "Niranjan",
-// 	Password: "12345678",
-// Categories: allCategories,
-
-// Categories: []model.Category{
-// 	{
-// 		Base: model.Base{
-// 			ID: uuid.NewV4(),
-// 		},
-// 		Name: "Social",
-// 		Bookmarks: []model.Bookmark{
-// 			{
-// 				Base: model.Base{
-// 					ID: uuid.NewV4(),
-// 				},
-// 				Description: "google",
-// 				URL:         "www.google.com",
-// 			},
-// {
-// 	Base: model.Base{
-// 		ID: uuid.NewV4(),
-// 	},
-// 	Description: "yahoo",
-// 	URL:         "www.yahoo.com",
-// },
-// },
-// }}
-// }
-// us.Register(&user)
-// cs.AddCategory(&category1)
-// bms.AddBookmark(&bookmark1)
-// bms.AddBookmark(&bookmark2)
-// fmt.Println("main", user.ID)
-// fmt.Println("this", cs, bms)
-// checkUser := &model.User{
-// 	Username: "Niranjan",
-// 	Password: "12345678",
-// }
-// eror := us.Login(checkUser)
-// fmt.Println(eror)
-// bookmark := model.Bookmark{
-// 	Description: "facebook",
-// 	URL: "www.facebook.com" ,
-
-// }
-// bms.AddBookmark()
