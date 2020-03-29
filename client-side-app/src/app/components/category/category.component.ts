a66eb8fab8c62a0b3ba39075f7d0df675ff1849c
Niranjan <shetty.niranjan4@gmail.com>
2020-03-29 18:33:26 +0530

    testing main page

--- client-side-app/src/app/components/category/category.component.ts
+++ client-side-app/src/app/components/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService, ICategory } from 'src/app/services/category-service/category.service';
 
 @Component({
   selector: 'bookmarkapp-category',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
+  categories: ICategory[]
+  errorOccured: boolean
+  errorMessage: string
+  category: ICategory
 
-  constructor() { }
+  constructor(private _service: CategoryService, ) { }
+
+  getAllCategories() {
+    console.log("called")
+    this._service.getAllCategories().subscribe((data: ICategory[]) => {
+      this.errorOccured = false
+      console.log("data", data)
+      this.categories = data
+    }, (error) => {
+      console.log("in 1st error:", error)
+      this.errorOccured = true
+      this.errorMessage = error
+
+    });
+  }
+
+
+  addCategory() {
+    this.category = { name: "angular", userID: sessionStorage.getItem('userid') }
+    console.log(this.category)
+    this._service.addCategory(this.category).subscribe((data: string) => {
+      console.log(data)
+
+    }, (error) => {
+      console.log(error)
+    })
+  }
 
   ngOnInit() {
+    // this.addCategory()
+    this.getAllCategories()
   }
 
 }
--- client-side-app/src/app/services/category-service/category.service.ts
+++ client-side-app/src/app/services/category-service/category.service.ts
@@ -10,23 +10,44 @@ import { Observable } from 'rxjs';
 })
 export class CategoryService {
   _baseURL: string
+  private categories: any
   constructor(private _http: HttpClient, private _route: Router) {
     this._baseURL = `${AppConstants.baseURL}/${sessionStorage.getItem("userid")}/category`
+    this.categories = []
   }
   getAllCategories(): Observable<ICategory[]> {
     return new Observable<ICategory[]>((observer) => {
       this._http.get(this._baseURL, {
-        headers: this.setTokenToHeader()
+        headers: new HttpHeaders().set("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZWRBdCI6MTU4NTQ4MzMzNCwidXNlcklEIjoiZDU1Zjg3M2EtZjFiOC00NmRmLThhNWItMWU4ZGEwMmJiMjk3IiwidXNlcm5hbWUiOiJOaXJhbmphbiIsInZhbGlkVGlsbCI6MTU4NTQ4NTEzNH0.RqeoYxxc2sNjPl_Uui2ORerP6k3fI8VRCDKi77bDk1w")
       }).subscribe((data: ICategory[]) => {
         observer.next(data)
+        console.log("data:", data)
       }, (error) => {
+        console.log("in error")
         observer.error(error.error)
       })
     })
   }
 
+
+  addCategory(category: ICategory): Observable<string> {
+    console.log(this.setTokenToHeader())
+    return new Observable<string>((observer) => {
+      this._http.post(this._baseURL, category,
+        {
+          headers: this.setTokenToHeader()
+        }
+      ).subscribe((data: string) => {
+        console.log(data)
+        observer.next(data)
+      }, (error) => {
+        observer.next(error.error)
+      })
+    })
+  }
+
   setTokenToHeader(): HttpHeaders {
-    return new HttpHeaders().set('token', sessionStorage.getByID('token'));
+    return new HttpHeaders().set("token", sessionStorage.getItem("token"))
   }
 }
 
@@ -34,8 +55,8 @@ export class CategoryService {
 
 
 export interface ICategory {
-  id: string,
+  id?: string,
   name: string,
-  bookmarks: IBookmark[],
+  bookmarks?: IBookmark[],
   userID: string
 }
