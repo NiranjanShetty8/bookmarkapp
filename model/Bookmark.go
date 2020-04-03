3018141abdb731756957ca99a909c75aaa3a7e82
Niranjan <shetty.niranjan4@gmail.com>
2020-04-03 21:16:49 +0530

    category and bookmark name modified to accept non-unique values

--- model/Bookmark.go
+++ model/Bookmark.go
@@ -5,7 +5,7 @@ import uuid "github.com/satori/go.uuid"
 //Represents a bookmark
 type Bookmark struct {
 	Base
-	Name       string    `gorm:"type:varchar(30);unique;not null" json:"name"`
+	Name       string    `gorm:"type:varchar(30);not null" json:"name"`
 	URL        string    `gorm:"not null" json:"url"`
 	CategoryID uuid.UUID `gorm:"type:varchar(36);not null" json:"categoryID"`
 }
--- model/Category.go
+++ model/Category.go
@@ -5,7 +5,7 @@ import uuid "github.com/satori/go.uuid"
 // Represents category
 type Category struct {
 	Base
-	Name      string     `gorm:"unique;not null;type:varchar(30)" json:"name"`
+	Name      string     `gorm:"not null;type:varchar(30)" json:"name"`
 	UserID    uuid.UUID  `gorm:"type:varchar(36);not null" json:"userID"`
 	Bookmarks []Bookmark `json:"bookmarks"`
 }
